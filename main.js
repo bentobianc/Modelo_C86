519e619d628173b1ac3972e549b68be0a5f0c68a
bento <bentobianchi8@gmail.com>
2024-03-15 13:48:47 -0300

    c86

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function newImage()
 {
-	
+fabric.Image.fromURL('Birthday.jpg', function(Img)
+{
+    blockImageObject = Img;
+    blockImageObject.scaleToWidth(700);
+    blockImageObject.scaleToHeight(510);
+    blockImageObject.set({
+        top:0,
+        left: 0
+    });
+    canvas.add(blockImageObject);
+}
+)	
 	
 }
 
 function playSound(){
-	
+	x.play();
 }
